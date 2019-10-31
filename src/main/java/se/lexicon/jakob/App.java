8accfeb2e494e20e785bb9c5c05e2321db7200e6
Jakob Persson <jakob.persson87@hotmail.com>
2019-10-31 10:59:38 +0100

    Added lucky numbers in Array.

--- src/main/java/se/lexicon/jakob/App.java
+++ src/main/java/se/lexicon/jakob/App.java
@@ -10,5 +10,14 @@ public class App
     {
         System.out.println("Hello Arrays!");
         System.out.println("Education for Arrays");
+
+        /*
+        int[] luckyNumber =  new int[3];
+        luckyNumber[0] = 8;
+        luckyNumber[1] = 8;
+        luckyNumber[2] = 8;
+        */
+
+        int[] luckyNumber = { 8, 17, 87};
     }
 }
